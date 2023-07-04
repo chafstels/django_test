1bb6bc5f8f58f618c2f75dad11b5494601f68de2
Anton <anton@Anton-MacBook-Pro.local>
2023-07-04 22:25:02 +0600

    add CRUD for university

--- config/urls.py
+++ config/urls.py
@@ -18,10 +18,15 @@ from django.contrib import admin
 from django.urls import path
 from post.views import PostAPIView, PostAPIList
 from shoop_books.views import users_list
+from university.views import StudentAPIList, StudentAPIUpdate, StudentAPIDetailView, CourseAPIList, CourseAPIUpdate, CourseAPIDetailView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/post/", PostAPIList.as_view()),
     path("api/v1/post/<int:pk>/", PostAPIView.as_view()),
-    path('users_list/', users_list)
+    path('users_list/', users_list),
+    path('api/v1/student/', StudentAPIList.as_view()),
+    path('api/v1/student/<int:pk>/', StudentAPIUpdate.as_view()),
+    path('api/v1/course/', CourseAPIList.as_view()),
+    path('api/v1/course/<int:pk>/', CourseAPIDetailView.as_view()),
 ]
--- university/views.py
+++ university/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from rest_framework import generics
+
+from .models import Student, Course
+from .serializer import StudentSerializer, CourseSerializer
+
 
 # Create your views here.
+
+class StudentAPIList(generics.ListCreateAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+class StudentAPIUpdate(generics.UpdateAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+class StudentAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+
+class CourseAPIList(generics.ListCreateAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
+class CourseAPIUpdate(generics.UpdateAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
+class CourseAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
